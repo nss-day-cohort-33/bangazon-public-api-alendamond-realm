c7c1bc70a89f03255ee30cea186cd8a1236d6a39
Amber Gooch <agooch91@gmail.com>
2019-10-04 14:34:23 -0500

    edited view for product type and added view for register

--- alendamond/urls.py
+++ alendamond/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from bangazonapi.models import *
+# from bangazonapi.models import *
+from bangazonapi.views import register_user, login_user
+from bangazonapi.views import ProductTypes
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'producttypes', ProductTypes, 'producttype')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^register$', register_user),
+    url(r'^login$', login_user),
     url(r'^api-token-auth/', obtain_auth_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
---
+++ bangazonapi/views/__init__.py
@@ -0,0 +1,8 @@
+from .register import register_user
+from .register import login_user
+# from .customer import Customers
+# from .order import Orders
+# from .paymenttype import PaymentTypes
+# from .product import Products
+from .producttype import ProductTypes
+# from .productrating import ProductRating
