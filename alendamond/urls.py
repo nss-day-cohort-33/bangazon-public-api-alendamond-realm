bb9f9a52f8259a75132e06e6bc4b58046432bd01
Curt Cato <ccato829@gmail.com>
2019-10-04 15:50:40 -0500

    for the brownies!

--- alendamond/urls.py
+++ alendamond/urls.py
@@ -2,11 +2,16 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from bangazonapi.models import *
+from bangazonapi.views import register_user, login_user, Orders
+
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'orders', Orders, 'order')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^register$', register_user),
+    url(r'^login$', login_user),
     url(r'^api-token-auth/', obtain_auth_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
---
+++ bangazonapi/views/__init__.py
@@ -0,0 +1,3 @@
+from .order import Orders
+from .register import register_user
+from .register import login_user
