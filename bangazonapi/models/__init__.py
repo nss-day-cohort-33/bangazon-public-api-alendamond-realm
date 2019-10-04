938c9e10aaca793163f36d909d4e2278bfbd590e
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-10-04 10:02:37 -0500

    changed names to singular/plural on models and fixtures as necessary

--- bangazonapi/models/__init__.py
+++ bangazonapi/models/__init__.py
@@ -1,6 +1,5 @@
 from .paymenttype import PaymentType
 from .order import Order
-from .customers import Customer
-from .order import Order
+from .customer import Customer
 from .productrating import ProductRating
 from .product import Product
