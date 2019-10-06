c5ef25cd92909500da119eeeb4df52eefc7175ac
Amber Gooch <agooch91@gmail.com>
2019-10-06 15:50:53 -0500

    fixed errors

--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -1,9 +1,8 @@
 from .register import register_user
 from .register import login_user
-from .customer import Customers
+# from .customer import Customers
 from .order import Orders
 from .paymenttype import PaymentTypes
 from .product import ProductData
-from .productrating import ProductRating
 from .producttype import ProductTypes
 
